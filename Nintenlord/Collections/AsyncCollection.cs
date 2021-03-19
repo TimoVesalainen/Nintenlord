db6363aa44a9f774176de7db5aaa1587a5501bcc
Timo Vesalainen <timovesalainen@hotmail.com>
2021-03-19 19:24:15 +0200

    Added collection for handling async tasks

---
+++ Nintenlord/Collections/AsyncCollection.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Nintenlord.Collections
+{
+    public sealed class AsyncCollection
+    {
+        private readonly List<Task> tasks = new List<Task>();
+
+        public void AddTask(Task task)
+        {
+            tasks.Add(task);
+        }
+
+        public async Task WaitAll()
+        {
+            while (tasks.Count > 0)
+            {
+                var finishedTask = await Task.WhenAny(tasks);
+                tasks.Remove(finishedTask);
+            }
+        }
+
+        public void ClearFinished()
+        {
+            for (int i = tasks.Count - 1; i >= 0; i--)
+            {
+                switch (tasks[i].Status)
+                {
+                    case TaskStatus.Faulted:
+                    case TaskStatus.Canceled:
+                    case TaskStatus.RanToCompletion:
+                        tasks.RemoveAt(i);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
