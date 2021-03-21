803213f89486652944f6339cd08c5db517ca6cff
Timo Vesalainen <timovesalainen@hotmail.com>
2021-03-21 15:54:47 +0200

    Added helper for values that are updated async

---
+++ Nintenlord/Utility/AsyncUpdaetableValue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Nintenlord.Utility
+{
+    public sealed class AsyncUpdaetableValue<T> : IUpdatableValue<T>
+    {
+        private readonly Func<Task<T>> updateFunc;
+        private Task settingTask;
+
+        public T Value { get; private set; }
+
+        public AsyncUpdaetableValue(Func<Task<T>> updateFunc, T startValue = default)
+        {
+            this.updateFunc = updateFunc;
+            this.Value = startValue;
+        }
+
+        public void NeedsUpdate()
+        {
+            async Task Update()
+            {
+                var newValue = await updateFunc();
+
+                Value = newValue;
+                settingTask = null;
+            }
+
+            if (settingTask == null)
+            {
+                settingTask = Update();
+            }
+        }
+
+        public static implicit operator AsyncUpdaetableValue<T>(T item)
+        {
+            return new AsyncUpdaetableValue<T>(async () => item, item);
+        }
+    }
+}
