71484465a270f7728b28d53223a43ddf861f4c5d
Timo Vesalainen <timovesalainen@hotmail.com>
2021-03-24 21:16:17 +0200

    One more test for confidence

--- Nintenlord.Tests/Collections/ArrayExtensions.cs
+++ Nintenlord.Tests/Collections/ArrayExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using NUnit.Framework;
-using System.Linq;
 using static Nintenlord.Collections.ArrayExtensions;
 
 namespace Nintenlord.Tests.Collections
@@ -23,6 +22,18 @@ namespace Nintenlord.Tests.Collections
                 new int[,] { { 0, 0 },{ 1, 0 } },
                 new int[,] { { 0, 0 },{ 0, 1 } }},
                 new[,] { { 1 } }.EmbedTo(1, 1, 0)
+            );
+        }
+
+        [Test]
+        public void EmbedOnceEachDimensionBigger()
+        {
+            Assert.AreEqual(new[] {
+                new int[,] { { 1, 2, 0 }, { 3, 4, 0 }, { 0, 0, 0 } },
+                new int[,] { { 0, 1, 2 }, { 0, 3, 4 }, { 0, 0, 0 } },
+                new int[,] { { 0, 0, 0 }, { 1, 2, 0 }, { 3, 4, 0 } },
+                new int[,] { { 0, 0, 0 }, { 0, 1, 2 }, { 0, 3, 4 } }},
+                new[,] { { 1, 2 }, { 3, 4 } }.EmbedTo(1, 1, 0)
             );
         }
     }
