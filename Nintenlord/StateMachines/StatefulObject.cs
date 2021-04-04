6f51305aa177b928a53ce7eabdbef618e77bde86
Timo Vesalainen <timovesalainen@hotmail.com>
2021-04-04 15:02:50 +0300

    Added stateful object

---
+++ Nintenlord/StateMachines/StatefulObject.cs
@@ -0,0 +1,26 @@
+﻿namespace Nintenlord.StateMachines
+{
+    public class StatefulObject<TState, TInput>
+    {
+        readonly IStateMachine<TState, TInput> stateMachine;
+        TState state;
+
+        public TState CurrentState => state;
+
+        public StatefulObject(IStateMachine<TState, TInput> stateMachine)
+        {
+            this.stateMachine = stateMachine;
+            state = stateMachine.StartState;
+        }
+
+        public void Update(TInput input)
+        {
+            state = stateMachine.Transition(state, input);
+        }
+
+        public bool IsFinished()
+        {
+            return stateMachine.IsFinalState(state);
+        }
+    }
+}
