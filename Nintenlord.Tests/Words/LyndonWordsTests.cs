53e7424bce08011a7df0d2d232992af6c8caece5
Timo Vesalainen <timovesalainen@hotmail.com>
2021-04-07 22:56:31 +0300

    Added code for generating and decomposing lyndon words

---
+++ Nintenlord.Tests/Words/LyndonWordsTests.cs
@@ -0,0 +1,70 @@
+﻿using Nintenlord.Words;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nintenlord.Tests.Words
+{
+    public class LyndonWordsTests
+    {
+        [Test]
+        public void TestLength4()
+        {
+            Assert.AreEqual(new int[][] { 
+                new int[] { 0 },
+                new int[] { 0,0,0,1 },
+                new int[] { 0,0,1 },
+                new int[] { 0,0,1,1 },
+                new int[] { 0,1 },
+                new int[] { 0,1,1 },
+                new int[] { 0,1,1,1 },
+                new int[] { 1 } 
+            },
+                LyndonWords.GetLyndonWords(2, 4));
+        }
+
+        [TestCase(1,2)]
+        [TestCase(2,19)]
+        [TestCase(3,11)]
+        [TestCase(4,7)]
+        [TestCase(5,4)]
+        [TestCase(6,2)]
+        public void TestFirstLast(int length, int alphabetSize)
+        {
+            var words = LyndonWords.GetLyndonWords(alphabetSize, length).ToArray();
+
+            Assert.AreEqual(new int[] { 0 }, words.First());
+            Assert.AreEqual(new int[] { alphabetSize - 1 }, words.Last());
+        }
+
+        [Test]
+        public void TestDecomposition() 
+        {
+            var word = new[] { 0, 1, 1, 0, 1, 0, 0, 1,0,1,1,1,0,0,0,0 };
+
+            var lyndonWords = LyndonWords.GetLyndonWords(2, word.Length).ToArray();
+
+            foreach (var item in LyndonWords.GetLyndonComposition(word))
+            {
+                var itemArray = item.ToArray();
+                Assert.IsTrue(lyndonWords.Any(lWord =>
+                {
+                    if (lWord.Length != itemArray.Length)
+                    {
+                        return false;
+                    }
+                    for (int i = 0; i < lWord.Length; i++)
+                    {
+                        if (itemArray[i] != lWord[i])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }), $"Expected {string.Join(",", itemArray)} to be Lyndon word");
+            }
+        }
+    }
+}
---
+++ Nintenlord/Words/LyndonWords.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nintenlord.Words
+{
+    public static class LyndonWords
+    {
+        public static IEnumerable<int[]> GetLyndonWords(int alphabetSize, int maxLength)
+        {
+            List<int> currentWord = new List<int>(maxLength);
+            currentWord.Add(0);
+            yield return currentWord.ToArray();
+
+            var finalAlphabet = alphabetSize - 1;
+            while (currentWord[0] != finalAlphabet)
+            {
+                var currentLength = currentWord.Count;
+                for (int i = currentLength; i < maxLength; i++)
+                {
+                    currentWord.Add(currentWord[i % currentLength]);
+                }
+                while (currentWord[currentWord.Count - 1] == finalAlphabet)
+                {
+                    currentWord.RemoveAt(currentWord.Count - 1);
+                }
+                currentWord[currentWord.Count - 1] = currentWord[currentWord.Count - 1] + 1;
+                yield return currentWord.ToArray();
+            }
+        }
+
+        /// <returns>Non-increasing sequence of Lyndon words</returns>
+        public static IEnumerable<IEnumerable<int>> GetLyndonComposition(IList<int> word)
+        {
+            int k = 0;
+            int m = 1;
+            int used = 0;
+            IEnumerable<int> GetLyndonWord(int length)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    yield return word[i + used];
+                }
+            }
+
+            while (m < word.Count && k < word.Count)
+            {
+                if (word[k] == word[m])
+                {
+                    k++;
+                    m++;
+                }
+                else if (word[k] < word[m])
+                {
+                    m++;
+                    k = used;
+                }
+                else //word[k] > word[m]
+                {
+                    var length = m - k;
+                    yield return GetLyndonWord(length);
+
+                    used += length;
+                    m = used + 1;
+                    k = used;
+                }
+                if (m == word.Count)
+                {
+                    var length = m - k;
+                    yield return GetLyndonWord(length);
+
+                    used += length;
+                    m = used + 1;
+                    k = used;
+                }
+            }
+            yield return GetLyndonWord(word.Count - used);
+        }
+    }
+}
