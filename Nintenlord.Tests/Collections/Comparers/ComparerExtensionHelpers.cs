cbc6d24336ac55143b661f5b3242588acf11144f
Timo Vesalainen <timovesalainen@hotmail.com>
2021-04-08 20:28:41 +0300

    Added helpers for comparisons

---
+++ Nintenlord.Tests/Collections/Comparers/ComparerExtensionHelpers.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Nintenlord.Collections.Comparers;
+
+namespace Nintenlord.Tests.Collections.Comparers
+{
+    public class ComparerExtensionHelpers
+    {
+        [Test]
+        public void TestMax()
+        {
+            Assert.AreEqual(1, Comparer<int>.Default.Max(1, 0));
+        }
+
+        [Test]
+        public void TestMin()
+        {
+            Assert.AreEqual(0, Comparer<int>.Default.Min(1, 0));
+        }
+    }
+}
---
+++ Nintenlord/Collections/Comparers/ComparerUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nintenlord.Collections.Comparers
+{
+    public static class ComparerUtils
+    {
+        public static T Max<T>(this IComparer<T> comparer, T item1, T item2)
+        {
+            var comparison = comparer.Compare(item1, item2);
+
+            if (comparison >= 0)
+            {
+                return item1;
+            }
+            else
+            {
+                return item2;
+            }
+        }
+
+        public static T Min<T>(this IComparer<T> comparer, T item1, T item2)
+        {
+            var comparison = comparer.Compare(item1, item2);
+
+            if (comparison <= 0)
+            {
+                return item1;
+            }
+            else
+            {
+                return item2;
+            }
+        }
+    }
+}
