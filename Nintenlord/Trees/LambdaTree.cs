63196ec664c676896187f65fb40f696dcfc8f350
Timo Vesalainen <timovesalainen@hotmail.com>
2021-04-16 19:29:35 +0300

    Added helper for trees

---
+++ Nintenlord/Trees/LambdaTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nintenlord.Trees
+{
+    public sealed class LambdaTree<TNode> : ITree<TNode>
+    {
+        private readonly Func<TNode, IEnumerable<TNode>> getChildren;
+
+        public LambdaTree(TNode root, Func<TNode, IEnumerable<TNode>> getChildren)
+        {
+            this.getChildren = getChildren;
+            Root = root;
+        }
+
+        public TNode Root { get; }
+
+        public IEnumerable<TNode> GetChildren(TNode node)
+        {
+            return getChildren(node);
+        }
+    }
+}
