8920ae172172601bb0aba1d2bd33a6f5dffc30d7
Timo Vesalainen <timovesalainen@hotmail.com>
2021-05-27 17:05:32 +0300

    Changed automatic tiling implementation

--- Nintenlord/Tilings/AutomaticTiling.cs
+++ Nintenlord/Tilings/AutomaticTiling.cs
@@ -7,26 +7,63 @@ namespace Nintenlord.Tilings
 {
     public sealed class AutomaticTiling<TState, TOutput> : ITiling<TOutput>
     {
-        readonly IStateMachine<TState, (int, int)> stateMachine;
+        readonly IStateMachine<TState, int> positiveXstateMachine;
+        readonly IStateMachine<TState, int> negativeXstateMachine;
+        readonly IStateMachine<TState, int> positiveYstateMachine;
+        readonly IStateMachine<TState, int> negativeYstateMachine;
         readonly int baseValue;
-        readonly Func<TState, TOutput> converter;
+        readonly Func<TState, TState, TOutput> converter;
 
         public TOutput this[int x, int y] => GetTile(x, y);
 
-        public AutomaticTiling(IStateMachine<TState, (int, int)> stateMachine, int baseValue, Func<TState, TOutput> converter)
+        public AutomaticTiling(
+            IStateMachine<TState, int> positiveXstateMachine,
+            IStateMachine<TState, int> negativeXstateMachine,
+            IStateMachine<TState, int> positiveYstateMachine,
+            IStateMachine<TState, int> negativeYstateMachine,
+            int baseValue,
+            Func<TState, TState, TOutput> converter)
         {
-            this.stateMachine = stateMachine;
+
+            this.positiveXstateMachine = positiveXstateMachine;
+            this.negativeXstateMachine = negativeXstateMachine;
+            this.positiveYstateMachine = positiveYstateMachine;
+            this.negativeYstateMachine = negativeYstateMachine;
             this.baseValue = baseValue;
             this.converter = converter;
         }
 
         private TOutput GetTile(int x, int y)
         {
-            var endState = stateMachine.RunUntilFinalState(
-                x.BaseNRepresentation(baseValue).Zip(y.BaseNRepresentation(baseValue), (xn,yn) => (xn, yn))
-                ).Last();
+            IStateMachine<TState, int> horizontalMachine;
+            int numberHorizontal;
+            if (x >= 0)
+            {
+                horizontalMachine = positiveXstateMachine;
+                numberHorizontal = x;
+            }
+            else
+            {
+                horizontalMachine = negativeXstateMachine;
+                numberHorizontal = -x;
+            }
+            TState horizontal = horizontalMachine.RunUntilFinalState(numberHorizontal.BaseNRepresentation(baseValue)).Last();
 
-            return converter(endState);
+            IStateMachine<TState, int> verticalMachine;
+            int numberVertical;
+            if (y >= 0)
+            {
+                verticalMachine = positiveYstateMachine;
+                numberVertical = y;
+            }
+            else
+            {
+                verticalMachine = negativeYstateMachine;
+                numberVertical = -y;
+            }
+            TState vertical = verticalMachine.RunUntilFinalState(numberVertical.BaseNRepresentation(baseValue)).Last();
+
+            return converter(horizontal, vertical);
         }
     }
 
