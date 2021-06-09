e3d68d1762815742c7963763e52838b321cde7d8
Timo Vesalainen <timovesalainen@hotmail.com>
2021-06-09 22:16:47 +0300

    Added helper to refine comparers

---
+++ Nintenlord/Collections/Comparers/SequentialComparer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nintenlord.Collections.Comparers
+{
+    public sealed class SequentialComparer<T> : IComparer<T>
+    {
+        readonly IComparer<T>[] comparers;
+
+        public SequentialComparer(IEnumerable<IComparer<T>> comparers)
+        {
+            if (comparers is null)
+            {
+                throw new ArgumentNullException(nameof(comparers));
+            }
+
+            this.comparers = comparers.ToArray();
+        }
+
+        public int Compare(T x, T y)
+        {
+            for (int i = 0; i < comparers.Length; i++)
+            {
+                var result = comparers[i].Compare(x, y);
+                if (result != 0)
+                {
+                    return result;
+                }
+            }
+            return 0;
+        }
+    }
+}
