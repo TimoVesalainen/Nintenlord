1ea5f8b000a09dfc44e9a202cd585bd40d61a773
Timo Vesalainen <timovesalainen@hotmail.com>
2021-06-12 12:59:09 +0300

    Added non-discriminating union of trees

---
+++ Nintenlord/Trees/UnionTree.cs
@@ -0,0 +1,27 @@
+﻿using Nintenlord.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nintenlord.Trees
+{
+    public sealed class UnionTree<TNode> : ITree<Maybe<TNode>>
+    {
+        private readonly ITree<TNode>[] trees;
+        private readonly Maybe<TNode>[] roots;
+
+        public UnionTree(IEnumerable<ITree<TNode>> trees)
+        {
+            this.trees = trees?.ToArray() ?? throw new ArgumentNullException(nameof(trees));
+            this.roots = trees.Select(tree => Maybe<TNode>.Just(tree.Root)).ToArray();
+        }
+
+        public Maybe<TNode> Root => Maybe<TNode>.Nothing;
+
+        public IEnumerable<Maybe<TNode>> GetChildren(Maybe<TNode> node)
+        {
+            return node.Select(realNode => trees.SelectMany(tree => tree.GetChildren(realNode).Select(Maybe<TNode>.Just)))
+                       .GetValueOrDefault(roots);
+        }
+    }
+}
