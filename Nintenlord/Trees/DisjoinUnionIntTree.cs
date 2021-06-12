31cbc38c79a6faa45395798f2e5801d32a672b53
Timo Vesalainen <timovesalainen@hotmail.com>
2021-06-12 13:05:00 +0300

    Added Int either and int tree disjoin union tree that doesn't use Either

---
+++ Nintenlord/Trees/DisjoinUnionIntTree.cs
@@ -0,0 +1,37 @@
+﻿using Nintenlord.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nintenlord.Trees
+{
+    /// <summary>
+    /// Version of <c>DisjoinUnionTree</c> that uses <c>IntEither</c>
+    /// </summary>
+    public sealed class DisjoinUnionIntTree : ITree<IntEither>
+    {
+        readonly ITree<int>[] trees;
+        readonly Func<int, IEnumerable<IntEither>>[] getChildren;
+        readonly IntEither[] treeRoots;
+        readonly IntEither root;
+
+        public IntEither Root => root;
+
+        public DisjoinUnionIntTree(IEnumerable<ITree<int>> trees)
+        {
+            this.trees = trees.ToArray();
+            this.treeRoots = this.trees.Select((t, i) => new IntEither(t.Root, i)).ToArray();
+
+            this.getChildren = this.trees.Select((t, i) => (Func<int, IEnumerable<IntEither>>)(n => t.GetChildren(n).Select(c => new IntEither(i, c))))
+                .Append(_ => treeRoots)
+                .ToArray();
+
+            root = new IntEither(0, this.trees.Length);
+        }
+
+        public IEnumerable<IntEither> GetChildren(IntEither node)
+        {
+            return node.Apply(getChildren);
+        }
+    }
+}
---
+++ Nintenlord/Trees/IntEither.cs
@@ -0,0 +1,53 @@
+﻿using Nintenlord.Utility;
+using System;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace Nintenlord.Trees
+{
+    [DataContract]
+    [DebuggerDisplay("[{value}: {index}]")]
+    public readonly struct IntEither
+    {
+        [DataMember]
+        private readonly int value;
+        [DataMember]
+        private readonly int index;
+
+        public IntEither(int value, int index)
+        {
+            this.value = value;
+            this.index = index;
+        }
+
+        public void Apply(Action<int>[] actions)
+        {
+            actions[index](value);
+        }
+
+        public T Apply<T>(Func<int, T>[] funcs)
+        {
+            return funcs[index](value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is IntEither either &&
+                   value == either.value &&
+                   index == either.index;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1432443131;
+            hashCode = hashCode * -1521134295 + value.GetHashCode();
+            hashCode = hashCode * -1521134295 + index.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{{Option{index}: {value}}}";
+        }
+    }
+}
