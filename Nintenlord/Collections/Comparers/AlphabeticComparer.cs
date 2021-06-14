639cae7d2921eee2cff25ddf9c10bde696192e93
Timo Vesalainen <timovesalainen@hotmail.com>
2021-06-14 21:45:45 +0300

    Added alphabetic and lexicographic ordering

---
+++ Nintenlord/Collections/Comparers/AlphabeticComparer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nintenlord.Collections.Comparers
+{
+    public sealed class AlphabeticComparer<T> : IComparer<IEnumerable<T>>
+    {
+        readonly LexicographicComparer<T> lexicographicComparer;
+
+        public AlphabeticComparer(IComparer<T> ordering)
+        {
+            lexicographicComparer = new LexicographicComparer<T>(ordering);
+        }
+
+        public int Compare(IEnumerable<T> x, IEnumerable<T> y)
+        {
+            var xLength = x.Count();
+            var yLength = y.Count();
+
+            if (xLength < yLength)
+            {
+                return -1;
+            }
+            else if (xLength > yLength)
+            {
+                return 1;
+            }
+            else
+            {
+                return lexicographicComparer.Compare(x, y);
+            }
+        }
+    }
+}
---
+++ Nintenlord/Collections/Comparers/LexicographicComparer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nintenlord.Collections.Comparers
+{
+    public sealed class LexicographicComparer<T> : IComparer<IEnumerable<T>>
+    {
+        readonly IComparer<T> ordering;
+
+        public LexicographicComparer(IComparer<T> ordering)
+        {
+            this.ordering = ordering;
+        }
+
+        public int Compare(IEnumerable<T> x, IEnumerable<T> y)
+        {
+            using (var xEnumerator = x.GetEnumerator())
+            {
+                using (var yEnumerator = y.GetEnumerator())
+                {
+                    bool xHasValue = xEnumerator.MoveNext();
+                    bool yHasValue = yEnumerator.MoveNext();
+                    while (xHasValue && yHasValue)
+                    {
+                        var charComparison = ordering.Compare(xEnumerator.Current, yEnumerator.Current);
+                        if (charComparison != 0)
+                        {
+                            return charComparison;
+                        }
+
+                        xHasValue = xEnumerator.MoveNext();
+                        yHasValue = yEnumerator.MoveNext();
+                    }
+
+                    if (xHasValue == yHasValue)
+                    {
+                        return 0;
+                    }
+                    else if (xHasValue)
+                    {
+                        return 1;
+                    }
+                    else if (yHasValue)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+            }
+        }
+    }
+}
