13c8a25936c7463b5642d023f8f4229c9a40867d
Timo Vesalainen <timovesalainen@hotmail.com>
2021-06-20 22:59:52 +0300

    Added helper for cached resource buffer

---
+++ Nintenlord/Utility/ResourceBufferer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Nintenlord.Utility
+{
+    public sealed class ResourceBufferer<T>
+    {
+        readonly Func<T> resourceCreator;
+
+        readonly ConcurrentQueue<T> createdResources;
+
+        public ResourceBufferer(Func<T> resourceCreator)
+        {
+            this.resourceCreator = resourceCreator ?? throw new ArgumentNullException(nameof(resourceCreator));
+            this.createdResources = new ConcurrentQueue<T>();
+        }
+
+        public Usage ClaimResource()
+        {
+            if (createdResources.TryDequeue(out var resource))
+            {
+                return new Usage(resource, this);
+            }
+            else
+            {
+                return new Usage(resourceCreator(), this);
+            }
+        }
+
+
+        public readonly struct Usage : IDisposable
+        {
+            public readonly T Resourse;
+            private readonly ResourceBufferer<T> owner;
+
+            public Usage(T resourse, ResourceBufferer<T> owner)
+            {
+                Resourse = resourse;
+                this.owner = owner;
+            }
+
+            public void Dispose()
+            {
+                owner.Release(Resourse);
+            }
+        }
+
+
+
+        private void Release(T resourse)
+        {
+            createdResources.Enqueue(resourse);
+        }
+    }
+}
