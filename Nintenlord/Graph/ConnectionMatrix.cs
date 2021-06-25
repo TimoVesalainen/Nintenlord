d502e9d40ec2f9b6afb0767e910a360e200523c9
Timo Vesalainen <timovesalainen@hotmail.com>
2021-06-25 13:33:05 +0300

    Added matrix for graph connectivity

---
+++ Nintenlord/Graph/ConnectionMatrix.cs
@@ -0,0 +1,23 @@
+﻿using Nintenlord.Matrix;
+using System.Linq;
+
+namespace Nintenlord.Graph
+{
+    public sealed class ConnectionMatrix<TNode> : IMatrix<bool>
+    {
+        readonly IGraph<TNode> graph;
+        readonly TNode[] nodeArray;
+
+        public ConnectionMatrix(IGraph<TNode> graph)
+        {
+            this.graph = graph;
+            this.nodeArray = graph.Nodes.ToArray();
+        }
+
+        public bool this[int x, int y] => graph.IsEdge(nodeArray[x], nodeArray[y]);
+
+        public int Width => nodeArray.Length;
+
+        public int Height => nodeArray.Length;
+    }
+}
