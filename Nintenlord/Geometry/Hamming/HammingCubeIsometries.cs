240b4abc561e2b193b81b8b833a54a2724b27b1e
Timo Vesalainen <timovesalainen@hotmail.com>
2021-06-29 11:37:05 +0300

    Added helper to enumerate all isometries of Haming cube

---
+++ Nintenlord/Geometry/Hamming/HammingCubeIsometries.cs
@@ -0,0 +1,42 @@
+﻿using Nintenlord.Trees;
+using Nintenlord.Utility.Primitives;
+using System;
+using System.Collections.Generic;
+
+namespace Nintenlord.Geometry.Hamming
+{
+    public static class HammingCubeIsometries
+    {
+        public static IEnumerable<Func<HammingCode, HammingCode>> GetIsometries(int dimensions)
+        {
+            var maxInvert = 1 << dimensions;
+
+            var permutationTree = PermutationTree.ForLength(dimensions);
+
+            foreach (var permutation in permutationTree.GetPaths())
+            {
+                for (int toInvert = 0; toInvert < maxInvert; toInvert++)
+                {
+                    HammingCode Isometry(HammingCode original)
+                    {
+                        var value = original.Index;
+
+                        for (int j = 1; j < permutation.Count; j++)
+                        {
+                            var (index1, index2) = permutation[j];
+
+                            if (index1 != index2)
+                            {
+                                value = value.SwapBits(index1, index2);
+                            }
+                        }
+
+                        return new HammingCode(value ^ toInvert, dimensions);
+                    }
+
+                    yield return Isometry;
+                }
+            }
+        }
+    }
+}
