acdf52a73d75c71b9c4bcd10a3367c5cef22fab0
Timo Vesalainen <timovesalainen@hotmail.com>
2021-09-20 19:59:05 +0300

    Added test for increasing subsequence getting

---
+++ Nintenlord.Tests/Collections/Lists/ListExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nintenlord.Collections.Lists;
+using NUnit.Framework;
+
+namespace Nintenlord.Tests.Collections.Lists
+{
+    public class ListExtensionsTests
+    {
+        [Test]
+        public void GetLongestIncreasingSubsequence()
+        {
+            var array = new[] { 0, 9, 1, 8, 2, 7, 3, 6, 4, 5 };
+
+            Assert.AreEqual(new[] { 0, 1, 2, 3, 4, 5 }, array.GetLongestIncreasingSubsequence().ToArray());
+        }
+    }
+}
