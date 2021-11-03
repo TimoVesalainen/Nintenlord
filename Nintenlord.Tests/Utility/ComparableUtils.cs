aef35ffac784e473b2f590fc0421aa9d5bad5397
Timo Vesalainen <timovesalainen@hotmail.com>
2021-11-03 21:55:46 +0200

    Added helpers for get min and max of comparables

---
+++ Nintenlord.Tests/Utility/ComparableUtils.cs
@@ -0,0 +1,47 @@
+﻿using Nintenlord.Utility;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintenlord.Tests.Utility
+{
+    public class ComparableUtilsTest
+    {
+        [Test]
+        public static void TestMin()
+        {
+            Assert.AreEqual(ComparableUtils.Min(0, 1), 0);
+            Assert.AreEqual(ComparableUtils.Min(1, 0), 0);
+
+            Assert.AreEqual(ComparableUtils.MinNullIsLargest<int>(1, 0), 0);
+            Assert.AreEqual(ComparableUtils.MinNullIsLargest<int>(0, null), 0);
+            Assert.AreEqual(ComparableUtils.MinNullIsLargest<int>(null, 0), 0);
+            Assert.AreEqual(ComparableUtils.MinNullIsLargest<int>(null, null), null);
+
+            Assert.AreEqual(ComparableUtils.MinNullIsSmallest<int>(1, 0), 0);
+            Assert.AreEqual(ComparableUtils.MinNullIsSmallest<int>(0, null), null);
+            Assert.AreEqual(ComparableUtils.MinNullIsSmallest<int>(null, 0), null);
+            Assert.AreEqual(ComparableUtils.MinNullIsSmallest<int>(null, null), null);
+        }
+
+        [Test]
+        public static void TestMax()
+        {
+            Assert.AreEqual(ComparableUtils.Max(0, 1), 1);
+            Assert.AreEqual(ComparableUtils.Max(1, 0), 1);
+
+            Assert.AreEqual(ComparableUtils.MaxNullIsLargest<int>(1, 0), 1);
+            Assert.AreEqual(ComparableUtils.MaxNullIsLargest<int>(0, null), null);
+            Assert.AreEqual(ComparableUtils.MaxNullIsLargest<int>(null, 0), null);
+            Assert.AreEqual(ComparableUtils.MaxNullIsLargest<int>(null, null), null);
+
+            Assert.AreEqual(ComparableUtils.MaxNullIsSmallest<int>(1, 0), 1);
+            Assert.AreEqual(ComparableUtils.MaxNullIsSmallest<int>(0, null), 0);
+            Assert.AreEqual(ComparableUtils.MaxNullIsSmallest<int>(null, 0), 0);
+            Assert.AreEqual(ComparableUtils.MaxNullIsSmallest<int>(null, null), null);
+        }
+    }
+}
---
+++ Nintenlord/Utility/ComparableUtils.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintenlord.Utility
+{
+    static public class ComparableUtils
+    {
+        public static T Min<T>(T a, T b) where T : IComparable<T>
+        {
+            var comparison = a.CompareTo(b);
+
+            if (comparison <= 0)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        public static T? MinNullIsLargest<T>(T? a, T? b) where T : struct, IComparable<T>
+        {
+            if (a is T aVal && b is T bVal)
+            {
+                return Min(aVal, bVal);
+            }
+            else
+            {
+                return a ?? b;
+            }
+        }
+
+        public static T? MinNullIsSmallest<T>(T? a, T? b) where T : struct, IComparable<T>
+        {
+            if (a is T aVal && b is T bVal)
+            {
+                return Min(aVal, bVal);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static T Max<T>(T a, T b) where T : IComparable<T>
+        {
+            var comparison = a.CompareTo(b);
+
+            if (comparison >= 0)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        public static T? MaxNullIsLargest<T>(T? a, T? b) where T : struct, IComparable<T>
+        {
+            if (a is T aVal && b is T bVal)
+            {
+                return Max(aVal, bVal);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static T? MaxNullIsSmallest<T>(T? a, T? b) where T : struct, IComparable<T>
+        {
+            if (a is T aVal && b is T bVal)
+            {
+                return Max(aVal, bVal);
+            }
+            else
+            {
+                return a ?? b;
+            }
+        }
+    }
+}
