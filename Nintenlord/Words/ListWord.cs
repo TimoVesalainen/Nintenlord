2bec3815c0399d264cca988ae479e146b0d24604
Timo Vesalainen <timovesalainen@hotmail.com>
2021-11-21 13:21:13 +0200

    Added helpers for frapping concrete classes

---
+++ Nintenlord/Words/ListWord.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Nintenlord.Words
+{
+    public sealed class ListWord<T> : IWord<T>
+    {
+        readonly IReadOnlyList<T> list;
+
+        public ListWord(IReadOnlyList<T> list)
+        {
+            this.list = list;
+        }
+
+        public T this[BigInteger index] => list[(int)index];
+
+        public BigInteger? Length => list.Count;
+    }
+}
---
+++ Nintenlord/Words/StringWord.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace Nintenlord.Words
+{
+    public sealed class StringWord : IWord<char>
+    {
+        readonly string text;
+
+        public StringWord(string text)
+        {
+            this.text = text;
+        }
+
+        public char this[BigInteger index] => text[(int)index];
+
+        public BigInteger? Length => text.Length;
+    }
+}
