35ee3fd1b5ee6f7c7e063b94a80adab8a8fe8aab
Timo Vesalainen <timovesalainen@hotmail.com>
2021-11-23 23:16:30 +0200

    Added field used in ListDiff

---
+++ Nintenlord/Geometry/SquareCorners.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nintenlord.Geometry
+{
+    public enum SquareCorners
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight
+    }
+
+    public static class SquareCornersHelpers
+    {
+        public static SquareCorners OppositeCorner(this SquareCorners corner)
+        {
+            switch (corner)
+            {
+                case SquareCorners.TopLeft:
+                    return SquareCorners.BottomRight;
+                case SquareCorners.TopRight:
+                    return SquareCorners.BottomLeft;
+                case SquareCorners.BottomLeft:
+                    return SquareCorners.TopRight;
+                case SquareCorners.BottomRight:
+                    return SquareCorners.TopLeft;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(corner), corner, "Invalid corner");
+            }
+        }
+    }
+}
