3e3b17643e5364e883d63942eabf22365325851c
Timo Vesalainen <timovesalainen@hotmail.com>
2021-11-24 22:43:47 +0200

    Added single state state maching

---
+++ Nintenlord/StateMachines/SingleStateMachine.cs
@@ -0,0 +1,19 @@
+﻿using System.Reactive;
+
+namespace Nintenlord.StateMachines
+{
+    public sealed class SingleStateMachine<T> : IStateMachine<Unit, T>
+    {
+        public Unit StartState => Unit.Default;
+
+        public bool IsFinalState(Unit state)
+        {
+            return false;
+        }
+
+        public Unit Transition(Unit currentState, T input)
+        {
+            return currentState;
+        }
+    }
+}
