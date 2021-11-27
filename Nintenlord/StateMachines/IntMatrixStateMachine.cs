8db3da878347e677be59822dea8b255964e15a70
Timo Vesalainen <timovesalainen@hotmail.com>
2021-11-27 21:10:36 +0200

    Added state machine based on matrix

---
+++ Nintenlord/StateMachines/IntMatrixStateMachine.cs
@@ -0,0 +1,27 @@
+﻿using Nintenlord.Matricis;
+using System;
+
+namespace Nintenlord.StateMachines
+{
+    public sealed class IntMatrixStateMachine : IStateMachine<int, int>
+    {
+        readonly IMatrix<int> matrix;
+        readonly Predicate<int> isFinal;
+
+        public IntMatrixStateMachine(IMatrix<int> matrix, Predicate<int> isFinal, int startState)
+        {
+            this.matrix = matrix;
+            this.isFinal = isFinal;
+            StartState = startState;
+        }
+
+        public int StartState { get; }
+
+        public bool IsFinalState(int state) => isFinal(state);
+
+        public int Transition(int currentState, int input)
+        {
+            return matrix[currentState, input];
+        }
+    }
+}
