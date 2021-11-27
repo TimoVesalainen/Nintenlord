acab7a4687f5cb40ee41837f2d932a93f1a5a052
Timo Vesalainen <timovesalainen@hotmail.com>
2021-11-27 21:10:02 +0200

    Added commands adder helper

---
+++ Nintenlord/StateMachines/AddCommandsStateMachine.cs
@@ -0,0 +1,33 @@
+﻿using Nintenlord.Utility;
+using System;
+
+namespace Nintenlord.StateMachines
+{
+    public sealed class AddCommandsStateMachine<TState, TInput1, TInput2> : IStateMachine<TState, Either<TInput1, TInput2>>
+    {
+        readonly IStateMachine<TState, TInput1> original;
+        readonly Func<TInput2, TState, TState> newTransitions;
+
+        public AddCommandsStateMachine(IStateMachine<TState, TInput1> original, Func<TInput2, TState, TState> newTransitions)
+        {
+            this.original = original;
+            this.newTransitions = newTransitions;
+        }
+
+        public TState StartState => original.StartState;
+
+        public bool IsFinalState(TState state)
+        {
+            return original.IsFinalState(state);
+        }
+
+        public TState Transition(TState currentState, Either<TInput1, TInput2> input)
+        {
+            return input.Apply(
+                originalInput => original.Transition(currentState, originalInput), 
+                newInput => newTransitions(newInput, currentState)
+                );
+
+        }
+    }
+}
