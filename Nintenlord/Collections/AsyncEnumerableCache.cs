ba94131a0c83ae225f66ead440950c51692e7611
Timo Vesalainen <timovesalainen@hotmail.com>
2022-08-29 19:42:48 +0300

    Added caches for enumerables

---
+++ Nintenlord/Collections/AsyncEnumerableCache.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nintenlord.Collections
+{
+    /// <summary>
+    /// Caches values from potentially infinite async enumerable
+    /// </summary>
+    public sealed class AsyncEnumerableCache<T> : IAsyncDisposable
+    {
+        readonly List<T> cachedItems = new();
+        readonly IAsyncEnumerator<T> enumerator;
+        readonly SemaphoreSlim semaphore = new(1, 1);
+
+        public Task<T> this[int index]
+        {
+            get
+            {
+                if (index <= cachedItems.Count)
+                {
+                    async Task<T> CacheAndReturn()
+                    {
+                        await semaphore.WaitAsync();
+                        await CacheValues(index);
+                        semaphore.Release();
+
+                        return cachedItems[index];
+                    }
+
+                    return CacheAndReturn();
+                }
+                else
+                {
+                    return Task.FromResult(cachedItems[index]);
+                }
+
+            }
+        }
+
+        async Task CacheValues(int toIndex)
+        {
+            while (toIndex <= cachedItems.Count)
+            {
+                if (await enumerator.MoveNextAsync())
+                {
+                    cachedItems.Add(enumerator.Current);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Enumerator doesn't have enough items");
+                }
+            }
+        }
+
+        public AsyncEnumerableCache(IAsyncEnumerable<T> items)
+        {
+            enumerator = items.GetAsyncEnumerator();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await enumerator.DisposeAsync();
+        }
+    }
+}
---
+++ Nintenlord/Collections/EnumerableCache.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nintenlord.Collections
+{
+    /// <summary>
+    /// Caches values from potentially infinite enumerable
+    /// </summary>
+    public sealed class EnumerableCache<T> : IDisposable
+    {
+        readonly List<T> cachedItems = new();
+        readonly IEnumerator<T> enumerator;
+
+        private bool disposedValue;
+
+        public T this[int index]
+        {
+            get
+            {
+                while (index <= cachedItems.Count)
+                {
+                    if (enumerator.MoveNext())
+                    {
+                        cachedItems.Add(enumerator.Current);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Enumerator doesn't have enough items");
+                    }
+                }
+                return cachedItems[index];
+            }
+        }
+
+        public EnumerableCache(IEnumerable<T> items)
+        {
+            enumerator = items.GetEnumerator();
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    enumerator.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
