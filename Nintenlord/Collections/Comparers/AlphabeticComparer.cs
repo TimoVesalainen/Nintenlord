98a7158edf9e62cac0397cd82a313f84baa47d16
Timo Vesalainen <timovesalainen@hotmail.com>
2023-07-25 14:26:36 +0300

    Rewordked alphabet comparison to only enumerate parameters once

--- Nintenlord/Collections/Comparers/AlphabeticComparer.cs
+++ Nintenlord/Collections/Comparers/AlphabeticComparer.cs
@@ -22,28 +22,76 @@ namespace Nintenlord.Collections.Comparers
         }
 
         readonly LexicographicComparer<T> lexicographicComparer;
+        private readonly IComparer<T> ordering;
 
         private AlphabeticComparer(IComparer<T> ordering)
         {
             lexicographicComparer = LexicographicComparer<T>.Create(ordering);
+            this.ordering = ordering;
         }
 
         public int Compare(IEnumerable<T> x, IEnumerable<T> y)
         {
-            var xLength = x.Count();
-            var yLength = y.Count();
-
-            if (xLength < yLength)
+            if (x is null)
             {
-                return -1;
+                throw new ArgumentNullException(nameof(x));
             }
-            else if (xLength > yLength)
+
+            if (y is null)
             {
-                return 1;
+                throw new ArgumentNullException(nameof(y));
+            }
+
+            if (x.TryGetNonEnumeratedCount(out var xCount) && y.TryGetNonEnumeratedCount(out var yCount))
+            {
+                if (xCount < yCount)
+                {
+                    return -1;
+                }
+                else if (xCount > yCount)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return lexicographicComparer.Compare(x, y);
+                }
             }
             else
             {
-                return lexicographicComparer.Compare(x, y);
+                using var xEnumerator = x.GetEnumerator();
+                using var yEnumerator = y.GetEnumerator();
+                bool xHasValue = xEnumerator.MoveNext();
+                bool yHasValue = yEnumerator.MoveNext();
+                int? comparison = null;
+                while (xHasValue && yHasValue)
+                {
+                    var charComparison = ordering.Compare(xEnumerator.Current, yEnumerator.Current);
+                    if (charComparison != 0)
+                    {
+                        comparison ??= charComparison;
+                    }
+
+                    xHasValue = xEnumerator.MoveNext();
+                    yHasValue = yEnumerator.MoveNext();
+                }
+
+                if (xHasValue == yHasValue)
+                {
+                    return comparison ?? 0;
+                }
+                else if (xHasValue)
+                {
+                    return 1;
+                }
+                else if (yHasValue)
+                {
+                    return -1;
+                }
+                else
+                {
+                    throw new Exception();
+                }
             }
         }
     }
