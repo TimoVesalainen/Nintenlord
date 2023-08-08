deb112a3bbba168fca4e3f4e1305acb53466ddab
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-08 17:54:11 +0300

    Added NeedlemanWunschMatrix algorithm

--- Nintenlord/Collections/Lists/Metrics.cs
+++ Nintenlord/Collections/Lists/Metrics.cs
@@ -1,9 +1,8 @@
 ﻿using Nintenlord.Matricis;
+using Nintenlord.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nintenlord.Collections.Lists
 {
@@ -69,6 +68,60 @@ namespace Nintenlord.Collections.Lists
             }
 
             return matrix;
+        }
+
+        public static IMatrix<int> NeedlemanWunschMatrix<T1, T2>(this IReadOnlyList<T1> first, IReadOnlyList<T2> second, Func<T1, T2, int> similarity, int gapPenalty = -1)
+        {
+            var matrix = new ArrayMatrix<int>(first.Count + 1, second.Count + 1);
+
+            for (int i = 0; i < matrix.Width; i++)
+            {
+                matrix[i, 0] = gapPenalty * i;
+            }
+            for (int j = 0; j < matrix.Height; j++)
+            {
+                matrix[0, j] = gapPenalty * j;
+            }
+            for (int i = 1; i < matrix.Width; i++)
+            {
+                for (int j = 1; j < matrix.Height; j++)
+                {
+                    var delete = matrix[i - 1, j] + gapPenalty;
+                    var insert = matrix[i, j - 1] + gapPenalty;
+                    int match = matrix[i - 1, j - 1] + similarity(first[i - 1], second[j - 1]);
+                    matrix[i, j] = Math.Max(Math.Max(delete, insert), match);
+                }
+            }
+
+            return matrix;
+        }
+
+        public static IEnumerable<(Maybe<T1>, Maybe<T2>)> NeedlemanWunschMatching<T1, T2>(this IReadOnlyList<T1> first, IReadOnlyList<T2> second, Func<T1, T2, int> similarity, int gapPenalty = -1)
+        {
+            var matrix = NeedlemanWunschMatrix(first, second, similarity, gapPenalty);
+
+            int i = first.Count;
+            int j = second.Count;
+
+            while (i > 0 || j > 0)
+            {
+                if (i > 0 && j > 0 && matrix[i, j] == matrix[i - 1, j - 1] + similarity(first[i -1], second[j - 1]))
+                {
+                    yield return (first[i - 1], second[j - 1]);
+                    i--;
+                    j--;
+                }
+                else if (i > 0 && matrix[i, j] == matrix[i - 1, j] + gapPenalty)
+                {
+                    yield return (first[i - 1], Maybe<T2>.Nothing);
+                    i--;
+                }
+                else
+                {
+                    yield return (Maybe<T1>.Nothing, second[j - 1]);
+                    j--;
+                }
+            }
         }
     }
 }
