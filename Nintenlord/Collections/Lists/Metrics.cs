39497b7c99dbae8f0075ce98e21fc9383e523a8f
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-08 17:14:06 +0300

    Added helper to calculate Levenshtein Matrix

--- Nintenlord/Collections/Lists/Metrics.cs
+++ Nintenlord/Collections/Lists/Metrics.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nintenlord.Matricis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,6 +37,38 @@ namespace Nintenlord.Collections.Lists
             }
 
             return buffer1[second.Count];
+        }
+
+        /// <summary>
+        /// Calculates the Levenshtein matrix of two lists.
+        /// </summary>
+        /// <remarks>Uses Wagner–Fischer algorithm</remarks>
+        /// <returns>Levenshtein matrix</returns>
+        public static IMatrix<int> LevenshteinMatrix<T>(this IReadOnlyList<T> first, IReadOnlyList<T> second,
+            Func<T, int> delCost, Func<T, int> insCost, Func<T, T, int> replaceCost)
+        {
+            var matrix = new ArrayMatrix<int>(first.Count + 1, second.Count + 1);
+
+            for (int i = 0; i < matrix.Width; i++)
+            {
+                matrix[i, 0] = i;
+            }
+            for (int j = 0; j < matrix.Height; j++)
+            {
+                matrix[0, j] = j;
+            }
+            for (int i = 1; i < matrix.Width; i++)
+            {
+                for (int j = 1; j < matrix.Height; j++)
+                {
+                    var deletionCost = matrix[i - 1, j] + delCost(first[i-1]);
+                    var insertionCost = matrix[i, j - 1] + insCost(second[j-1]);
+                    int substitutionCost = matrix[i - 1, j - 1] + replaceCost(first[i - 1], second[j - 1]);
+                    matrix[i, j] = Math.Min(Math.Min(deletionCost, insertionCost), substitutionCost);
+                }
+            }
+
+            return matrix;
         }
     }
 }
