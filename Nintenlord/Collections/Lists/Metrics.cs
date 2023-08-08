39127ebb52be8e6a5ba99d8053d9bc1000075351
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-08 20:09:38 +0300

    Added SmithWaterman algorith

--- Nintenlord/Collections/Lists/Metrics.cs
+++ Nintenlord/Collections/Lists/Metrics.cs
@@ -158,5 +158,107 @@ namespace Nintenlord.Collections.Lists
 
             return matrix[first.Count, second.Count];
         }
+
+        public static IMatrix<int> SmithWatermanMatrix<T1, T2>(this IReadOnlyList<T1> first, IReadOnlyList<T2> second,
+            Func<T1, T2, int> similarity, Func<int, int> gapPenalty)
+        {
+            var matrix = new ArrayMatrix<int>(first.Count + 1, second.Count + 1);
+
+            for (int i = 0; i < matrix.Width; i++)
+            {
+                matrix[i, 0] = 0;
+            }
+            for (int j = 0; j < matrix.Height; j++)
+            {
+                matrix[0, j] = 0;
+            }
+
+            for (int i = 1; i < matrix.Width; i++)
+            {
+                for (int j = 1; j < matrix.Height; j++)
+                {
+                    var alingingScore = matrix[i - 1, j - 1] + similarity(first[i - 1], second[j - 1]);
+                    var hGap = Enumerable.Range(0, i).Select(k => matrix[i - k - 1, j] + gapPenalty(k + 1)).Max();
+                    var hvGap = Enumerable.Range(0, j).Select(l => matrix[i, j - l - 1] + gapPenalty(l + 1)).Max();
+
+                    matrix[i, j] = Math.Max(Math.Max(0, alingingScore), Math.Max(hGap, hvGap));
+                }
+            }
+
+            return matrix;
+        }
+
+        private static IEnumerable<(Maybe<T1>, Maybe<T2>)> Traceback<T1, T2>(IMatrix<int> matrix, IReadOnlyList<T1> first, IReadOnlyList<T2> second)
+        {
+            var (x, y, _) = matrix.FindMax();
+
+            while (x > 0 || y > 0)
+            {
+                if (matrix[x, y] == 0)
+                {
+                    break;
+                }
+
+                if (x > 0 && y > 0)
+                {
+                    var left = matrix[x - 1, y];
+                    var top = matrix[x, y - 1];
+                    var topLeft = matrix[x - 1, y - 1];
+                    if (topLeft == 0)
+                    {
+                        yield return (first[x], second[y]);
+                        x--;
+                        y--;
+                    }
+                    else if (left == 0)
+                    {
+                        yield return (first[x], Maybe<T2>.Nothing);
+                        x--;
+                    }
+                    else if (top == 0)
+                    {
+                        yield return (Maybe<T1>.Nothing, second[y]);
+                        y--;
+                    }
+                    else if (topLeft >= top && topLeft >= left)
+                    {
+                        yield return (first[x], second[y]);
+                        x--;
+                        y--;
+                    }
+                    else if (left >= top && left >= topLeft)
+                    {
+                        yield return (first[x], Maybe<T2>.Nothing);
+                        x--;
+                    }
+                    else // (top >= left && top >= topLeft)
+                    {
+                        yield return (Maybe<T1>.Nothing, second[y]);
+                        y--;
+                    }
+                }
+                else if (x > 0)
+                {
+                    yield return (first[x], Maybe<T2>.Nothing);
+                    x--;
+                }
+                else // (y > 0)
+                {
+                    yield return (Maybe<T1>.Nothing, second[y]);
+                    y--;
+                }
+            }
+        }
+
+        public static IEnumerable<(Maybe<T1>, Maybe<T2>)> SmithWatermanMatching<T1, T2>(this IReadOnlyList<T1> first, IReadOnlyList<T2> second,
+            Func<T1, T2, int> similarity, Func<int, int> gapPenalty)
+        {
+            var matrix = SmithWatermanMatrix(first, second, similarity, gapPenalty);
+
+            var list = new List<(Maybe<T1>, Maybe<T2>)>(first.Count + second.Count);
+            list.AddRange(Traceback(matrix, first, second));
+            list.Reverse();
+            return list;
+        }
     }
 }
