0a14575fe8c36414988dfac9d536a02e8e323551
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-12 18:11:08 +0300

    Added ImmutableHashSetFolder

---
+++ Nintenlord/Collections/Foldable/ImmutableHashSetFolder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Immutable;
+
+namespace Nintenlord.Collections.Foldable
+{
+    public sealed class ImmutableHashSetFolder<T> : IFolder<T, ImmutableHashSet<T>, ImmutableHashSet<T>>
+    {
+        public readonly static ImmutableHashSetFolder<T> Instance = new();
+
+        private ImmutableHashSetFolder() { }
+
+        public ImmutableHashSet<T> Start => ImmutableHashSet<T>.Empty;
+
+        public ImmutableHashSet<T> Fold(ImmutableHashSet<T> state, T input)
+        {
+            return state.Add(input);
+        }
+
+        public ImmutableHashSet<T> Transform(ImmutableHashSet<T> state)
+        {
+            return state;
+        }
+    }
+}
