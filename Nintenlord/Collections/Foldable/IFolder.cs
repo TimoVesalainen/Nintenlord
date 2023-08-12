a592ff74a7de11cd170d17c8fa6aa83d6642db2f
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-12 12:22:27 +0300

    Added foldable interface

---
+++ Nintenlord/Collections/Foldable/IFolder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintenlord.Collections.Foldable
+{
+    /// <summary>
+    /// Inspired by: https://github.com/Gabriella439/foldl
+    /// </summary>
+    public interface IFolder<TIn, TState, TOut>
+    {
+        TState Start { get; }
+
+        TState Fold(TState state, TIn input);
+
+        TOut Transform(TState state);
+    }
+}
