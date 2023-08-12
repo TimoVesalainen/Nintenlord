26a5657ae73a793f44e64c05e919a50b1e1f76ac
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-12 13:00:42 +0300

    Added folders for products and basic boolean operators

--- Nintenlord/Collections/Foldable/Folders.cs
+++ Nintenlord/Collections/Foldable/Folders.cs
@@ -4,10 +4,18 @@ namespace Nintenlord.Collections.Foldable
 {
     public static class Folders
     {
+        public static readonly IFolder<bool, bool, bool> And = new FunctionFolder<bool, bool, bool>(true, (x, y) => x && y, x => x);
+        public static readonly IFolder<bool, bool, bool> Or = new FunctionFolder<bool, bool, bool>(false, (x, y) => x || y, x => x);
+
         public static readonly IFolder<int, int, int> SumI = new FunctionFolder<int, int, int>(0, (x, y) => x + y, x => x);
         public static readonly IFolder<long, long, long> SumL = new FunctionFolder<long, long, long>(0, (x, y) => x + y, x => x);
         public static readonly IFolder<float, float, float> SumF = new FunctionFolder<float, float, float>(0, (x, y) => x + y, x => x);
         public static readonly IFolder<double, double, double> SumD = new FunctionFolder<double, double, double>(0, (x, y) => x + y, x => x);
+
+        public static readonly IFolder<int, int, int> ProductI = new FunctionFolder<int, int, int>(0, (x, y) => x * y, x => x);
+        public static readonly IFolder<long, long, long> ProductL = new FunctionFolder<long, long, long>(0, (x, y) => x * y, x => x);
+        public static readonly IFolder<float, float, float> ProductF = new FunctionFolder<float, float, float>(0, (x, y) => x * y, x => x);
+        public static readonly IFolder<double, double, double> ProductD = new FunctionFolder<double, double, double>(0, (x, y) => x * y, x => x);
 
         public static CountIntFolder<T> CountI<T>() => CountIntFolder<T>.Value;
         public static CountLongFolder<T> CountL<T>() => CountLongFolder<T>.Value;
@@ -24,6 +32,5 @@ namespace Nintenlord.Collections.Foldable
         {
             return Min<T>().Combine(Max<T>(), (x, y) => MaybeHelpers.Zip(x, y, (a, b) => (a, b)));
         }
-
     }
 }
