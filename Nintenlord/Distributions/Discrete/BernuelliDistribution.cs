56a1d125cc71eea15adebf77d7cc6ecf62b91415
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-26 16:53:01 +0300

    Removed unused usings

--- Nintenlord/Distributions/Discrete/BernuelliDistribution.cs
+++ Nintenlord/Distributions/Discrete/BernuelliDistribution.cs
@@ -3,8 +3,6 @@ using Nintenlord.Numerics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nintenlord.Distributions.Discrete
 {
