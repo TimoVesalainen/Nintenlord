82c3360f363254499ecf144f9e06f18dd7520a4b
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-26 21:02:05 +0300

    Updated formatting code

--- Nintenlord/Numerics/Formatting.cs
+++ Nintenlord/Numerics/Formatting.cs
@@ -1,49 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Numerics;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Nintenlord.Numerics
 {
     static public class Formatting
     {
-        public static string ToHexString(this int i, string prefix)
+        public static string ToBinString<TNumber>(this TNumber number, string postfix)
+            where TNumber : IBinaryInteger<TNumber>
         {
-            return i.ToHexString(prefix, "");
-        }
-        public static string ToHexString(this int i, string prefix, string postfix)
-        {
-            return prefix + Convert.ToString(i, 16).ToUpper() + postfix;
+            return number.ToBinString("", postfix);
         }
 
-        public static string ToBinString(this int i, string postfix)
+        public static string ToBinString<TNumber>(this TNumber number, string prefix = "", string postfix = "b")
+            where TNumber : IBinaryInteger<TNumber>
         {
-            return i.ToBinString("", postfix);
-        }
-        public static string ToBinString(this int i, string prefix, string postfix)
-        {
-            return prefix + Convert.ToString(i, 2) + postfix;
+            return prefix + number.ToString("B", CultureInfo.InvariantCulture) + postfix;
         }
 
-        public static string ToHexString(this byte i, string prefix)
+        public static string ToHexString<TNumber>(this TNumber number, string prefix)
+            where TNumber : IBinaryInteger<TNumber>
         {
-            return prefix + Convert.ToString(i, 16).ToUpper();
+            return number.ToHexString(prefix, "");
         }
 
-
-        public static string ToString(this byte[] i, int bytesPerWord)
+        public static string ToHexString<TNumber>(this TNumber number, string prefix, string postfix)
+            where TNumber : IBinaryInteger<TNumber>
         {
-            StringBuilder result = new StringBuilder();
-            for (int j = 0; j < i.Length; j++)
+            return prefix + number.ToString("X", CultureInfo.InvariantCulture) + postfix;
+        }
+
+        public static string ToString<TNumber>(this TNumber[] numbers, int numbersPerWord)
+            where TNumber : IBinaryInteger<TNumber>
+        {
+            var separators = Enumerable.Range(0, numbers.Length).Select(j => j % numbersPerWord == numbersPerWord - 1 ? " " : "");
+
+            return numbers.ToString(separators);
+        }
+
+        public static string ToString<TNumber>(this TNumber[] numbers, IEnumerable<string> separators)
+            where TNumber : IBinaryInteger<TNumber>
+        {
+            var length = TNumber.PopCount(TNumber.AllBitsSet);
+            var lengthBase16 = length >> 4;
+            if (lengthBase16 << 4 != length)
             {
-                result.Append(i[j].ToHexString("").PadLeft(2, '0'));
-                if (j % bytesPerWord == bytesPerWord - 1)
-                {
-                    result.Append(" ");
-                }
+                lengthBase16++;
             }
+            var lengthInt = int.CreateChecked(lengthBase16);
+
+            var result = new StringBuilder((numbers.Length + 1) * lengthInt);
+
+            var t = numbers
+                .Select(number => number.ToHexString("").PadLeft(lengthInt, '0'))
+                .Zip(separators, (number, sep) => new[] { number, sep }).SelectMany(x => x)
+                .SkipLast(1);
+
+            foreach (var text in t)
+            {
+                result.Append(text);
+            }
+
             return result.ToString();
         }
+
     }
 }
