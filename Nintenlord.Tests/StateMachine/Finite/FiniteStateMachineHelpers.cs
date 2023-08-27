d81855ebf066f77beb561677e497b7651d70760c
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-27 13:44:51 +0300

    Added test for equivalent state finding

---
+++ Nintenlord.Tests/StateMachine/Finite/FiniteStateMachineHelpers.cs
@@ -0,0 +1,30 @@
+﻿using Nintenlord.StateMachines.Finite;
+using NUnit.Framework;
+
+namespace Nintenlord.Tests.StateMachine.Finite
+{
+    internal class FiniteStateMachineHelpers
+    {
+        [Test]
+        public static void TestEquivalenceFindingEvenNumberOfAs()
+        {
+            var evenAmountOfCharacterAs = new DictionaryStateMachine<int, char>.Builder()
+                .AddState(0)
+                .AddState(1)
+                .AddState(2)
+                .AddState(3)
+                .AddTransition(0, 'a', 1)
+                .AddTransition(1, 'a', 2)
+                .AddTransition(2, 'a', 3)
+                .AddTransition(3, 'a', 0)
+                .SetStartState(0)
+                .SetIsFinalState(0, true)
+                .SetIsFinalState(2, true)
+                .Build();
+
+            var equivalent = evenAmountOfCharacterAs.FindEquivalentStates("a");
+
+            Assert.AreEqual(equivalent, new[] { new[] { 0, 2 }, new[] { 1, 3 } });
+        }
+    }
+}
