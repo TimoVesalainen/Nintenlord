2eabd55bb006a4b26de16e72bbf632ce6342c008
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-27 13:32:59 +0300

    Added equality comparer that only cares about length

---
+++ Nintenlord/Collections/EqualityComparer/ByLengthComparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintenlord.Collections.EqualityComparer
+{
+    public sealed class ByLengthComparer<T> : IEqualityComparer<IEnumerable<T>>
+    {
+        readonly IEqualityComparer<int> comparer = EqualityComparer<int>.Default;
+
+        public static readonly ByLengthComparer<T> Instance = new();
+
+        private ByLengthComparer()
+        {
+        }
+
+        public bool Equals(IEnumerable<T> x, IEnumerable<T> y)
+        {
+            return comparer.Equals(x.Count(), y.Count());
+        }
+
+        public int GetHashCode([DisallowNull] IEnumerable<T> obj)
+        {
+            return obj.Count();
+        }
+    }
+}
