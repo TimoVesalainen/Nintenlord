2c2badd7dee4027b51b752849ec81a00479d2483
Timo Vesalainen <timovesalainen@hotmail.com>
2023-08-27 14:02:31 +0300

    Added another test case

--- Nintenlord.Tests/StateMachine/Finite/FiniteStateMachineHelpers.cs
+++ Nintenlord.Tests/StateMachine/Finite/FiniteStateMachineHelpers.cs
@@ -26,5 +26,44 @@ namespace Nintenlord.Tests.StateMachine.Finite
 
             Assert.AreEqual(equivalent, new[] { new[] { 0, 2 }, new[] { 1, 3 } });
         }
+
+        [Test]
+        public static void TestEquivalenceFindingB()
+        {
+            var containsB = new DictionaryStateMachine<int, char>.Builder()
+                .AddState(0)
+                .AddState(1)
+                .AddState(2)
+                .AddState(3)
+                .AddState(4)
+                .AddState(5)
+                .AddState(6)
+                .AddState(7)
+                .AddTransition(0, 'a', 1)
+                .AddTransition(1, 'a', 2)
+                .AddTransition(2, 'a', 3)
+                .AddTransition(3, 'a', 0)
+                .AddTransition(0, 'b', 4)
+                .AddTransition(1, 'b', 5)
+                .AddTransition(2, 'b', 6)
+                .AddTransition(3, 'b', 7)
+                .AddTransition(4, 'a', 5)
+                .AddTransition(5, 'a', 6)
+                .AddTransition(6, 'a', 7)
+                .AddTransition(7, 'a', 4)
+                .SetStartState(0)
+                .SetIsFinalState(4, true)
+                .SetIsFinalState(5, true)
+                .SetIsFinalState(6, true)
+                .SetIsFinalState(7, true)
+                .Build();
+
+            var equivalent = containsB.FindEquivalentStates("ab");
+
+            Assert.AreEqual(equivalent, new[] {
+                new[] { 0, 1, 2, 3 },
+                new[] { 4, 5, 6, 7 }
+            });
+        }
     }
 }
