8ff2a161d39eea80fc09fd600ae081a333522e54
Timo Vesalainen <timovesalainen@hotmail.com>
2023-09-03 20:19:30 +0300

    Added incidency matrix type

---
+++ Nintenlord/Matricis/IncidencyMatrix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintenlord.Matricis
+{
+    public sealed class IncidencyMatrix<T> : IMatrix<int>
+    {
+        readonly Func<T, IEnumerable<T>> morphism;
+        readonly T[] values;
+        readonly IEqualityComparer<T> comparer;
+
+        public IncidencyMatrix(Func<T, IEnumerable<T>> morphism, IEnumerable<T> values, IEqualityComparer<T> comparer = null)
+        {
+            this.morphism = morphism ?? throw new ArgumentNullException(nameof(morphism));
+            this.values = values?.ToArray() ?? throw new ArgumentNullException(nameof(values));
+            this.comparer = comparer ?? EqualityComparer<T>.Default;
+        }
+
+        public int this[int x, int y] => morphism(values[y]).Count(item => comparer.Equals(values[x], item));
+
+        public int Width => values.Length;
+
+        public int Height => values.Length;
+    }
+}
