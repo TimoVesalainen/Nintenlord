c282bb71b56ea4258937292891b8d48568cfd443
Timo Vesalainen <timovesalainen@hotmail.com>
2023-09-11 21:01:19 +0300

    Uncommented code

--- Nintenlord.Windows/BitmapHelpers.cs
+++ Nintenlord.Windows/BitmapHelpers.cs
@@ -5,7 +5,6 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 
-/*
 namespace Nintenlord.Drawing
 {
     public static class BitmapHelpers
@@ -556,4 +555,3 @@ namespace Nintenlord.Drawing
         #endregion
     }
 }
-*/
