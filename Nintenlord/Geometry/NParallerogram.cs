af02fd4ac750693987dfc0bfb0dd34f4a5797544
Timo Vesalainen <timovesalainen@hotmail.com>
2023-09-11 22:49:14 +0300

    Renamed type and split to own file

---
+++ Nintenlord/Geometry/NParallerogram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Nintenlord.Geometry
+{
+    public readonly struct NParallerogram<TVector>
+        where TVector : IAdditionOperators<TVector, TVector, TVector>
+    {
+        public TVector Start => start;
+
+        private readonly TVector start;
+        readonly TVector[] edges;
+
+        public IEnumerable<TVector> Ends => GetEnds();
+        public IEnumerable<TVector> Edges => edges;
+        public TVector Opposite => GetOpposite();
+
+        public NParallerogram(TVector start, TVector[] edges)
+        {
+            this.start = start;
+            this.edges = edges ?? throw new ArgumentNullException(nameof(edges));
+        }
+
+        private IEnumerable<TVector> GetEnds()
+        {
+            var start = this.start;
+            return edges.Select(edge => start + edge);
+        }
+        private TVector GetOpposite()
+        {
+            var start = this.start;
+            return edges.Aggregate(start, (aggregate, edge) => aggregate + edge);
+        }
+    }
+}
--- Nintenlord/Geometry/SplitTree.cs
+++ Nintenlord/Geometry/SplitTree.cs
@@ -1,45 +1,21 @@
 ﻿using Nintenlord.Collections;
 using Nintenlord.Trees;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 
 namespace Nintenlord.Geometry
 {
-    public static class SplitTree
+    public static partial class SplitTree
     {
-        public readonly struct Node<TVector>
-            where TVector : IAdditionOperators<TVector, TVector, TVector>
-        {
-            public TVector Start => start;
 
-            private readonly TVector start;
-            readonly TVector[] edges;
-
-            public IEnumerable<TVector> Ends => GetEnds();
-            public IEnumerable<TVector> Edges => edges;
-
-            public Node(TVector start, TVector[] edges)
-            {
-                this.start = start;
-                this.edges = edges ?? throw new ArgumentNullException(nameof(edges));
-            }
-
-            private IEnumerable<TVector> GetEnds()
-            {
-                var start = this.start;
-                return edges.Select(edge => start + edge);
-            }
-        }
-
-        public static ITree<Node<TVector>> GetTree<TVector>(TVector start, TVector[] edges)
+        public static ITree<NParallerogram<TVector>> GetTree<TVector>(TVector start, TVector[] edges)
             where TVector : IAdditionOperators<TVector, TVector, TVector>,
             IDivisionOperators<TVector, double, TVector>
         {
             var edgeCount = edges.Length;
 
-            IEnumerable<Node<TVector>> GetChildren(Node<TVector> node)
+            IEnumerable<NParallerogram<TVector>> GetChildren(NParallerogram<TVector> node)
             {
                 var nextEdges = node.Edges.Select(edge => edge / 2).ToArray();
                 return BitArrayHelpers.GetAllArrays(edgeCount).Select(bits => {
@@ -48,11 +24,11 @@ namespace Nintenlord.Geometry
                     .Where(t => t.include)
                     .Aggregate(node.Start, (accum, t) =>  accum + t.end);
 
-                    return new Node<TVector>(start, nextEdges);
+                    return new NParallerogram<TVector>(start, nextEdges);
                 });
             }
 
-            return new LambdaTree<Node<TVector>>(new Node<TVector>(start, edges), GetChildren);
+            return new LambdaTree<NParallerogram<TVector>>(new NParallerogram<TVector>(start, edges), GetChildren);
         }
     }
 }
