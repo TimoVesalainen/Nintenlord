91b020bce5488a9ea966dfb1cba2a1ab0ea43302
Timo Vesalainen <timovesalainen@hotmail.com>
2023-09-24 19:46:19 +0300

    Added helper to get nth or default

--- Nintenlord/Collections/EnumerableExtensions.cs
+++ Nintenlord/Collections/EnumerableExtensions.cs
@@ -172,6 +172,21 @@ namespace Nintenlord.Collections
             return collection.Skip(index).First();
         }
 
+        public static T GetNthOrDefault<T>(this IEnumerable<T> collection, int index, T defaultValue = default)
+        {
+            if (collection is null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), index, "Negative value");
+            }
+
+            return collection.Skip(index).FirstOrDefault(defaultValue);
+        }
+
         public static string ToElementWiseString<T>(this IEnumerable<T> collection, string separator = ", ", string beginning = "{", string end = "}")
         {
             if (collection is null)
