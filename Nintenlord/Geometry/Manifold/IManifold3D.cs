d5237c07d495dc80b559f2e7eb41a845bf843885
Timo Vesalainen <timovesalainen@hotmail.com>
2023-11-14 20:41:27 +0200

    Added interface for manifolds

---
+++ Nintenlord/Geometry/Manifold/IManifold3D.cs
@@ -0,0 +1,15 @@
+﻿using Nintenlord.Matricis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nintenlord.Geometry.Manifold
+{
+    public interface IManifold3D<T>
+    {
+        bool FromTangentSpace(T basePoint, Vector3 tangent, out T otherPoint);
+        bool ToTangentSpace(T basePoint, T other, out Vector3 diff);
+    }
+}
