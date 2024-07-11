dfccf0adbf16850b4999dcd15d710256b3821009
Timo Vesalainen <timovesalainen@hotmail.com>
2024-07-11 10:01:17 +0300

    Added few helpers for discrete distribution combiners

---
+++ Nintenlord/Distributions/Discrete/ArrayDiscreteDistributions.cs
@@ -0,0 +1,46 @@
+﻿using Nintenlord.Collections;
+using Nintenlord.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nintenlord.Distributions.Discrete
+{
+    public sealed class ArrayDiscreteDistributions<T> : IDiscreteDistribution<T[]>
+    {
+        readonly IDiscreteDistribution<T> distribution;
+        readonly int count;
+
+        public ArrayDiscreteDistributions(IDiscreteDistribution<T> distribution, int count)
+        {
+            this.distribution = distribution ?? throw new ArgumentNullException(nameof(distribution));
+            if (count < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+            this.count = count;
+        }
+
+        public T[] Sample()
+        {
+            var sample = new T[count];
+
+            for (int i = 0; i < sample.Length; i++)
+            {
+                sample[i] = distribution.Sample();
+            }
+
+            return sample;
+        }
+
+        public IEnumerable<T[]> Support()
+        {
+            return Enumerable.Repeat(distribution.Support(), count).CartesianProduct().Select(item => item.ToArray());
+        }
+
+        public int Weight(T[] item)
+        {
+            return item.Select(distribution.Weight).Product();
+        }
+    }
+}
---
+++ Nintenlord/Distributions/Discrete/DiscreteDistributions.cs
@@ -0,0 +1,33 @@
+﻿using Nintenlord.Collections;
+using Nintenlord.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nintenlord.Distributions.Discrete
+{
+    public sealed class DiscreteDistributions<T> : IDiscreteDistribution<IEnumerable<T>>
+    {
+        readonly IDiscreteDistribution<T>[] distributions;
+
+        public DiscreteDistributions(IEnumerable<IDiscreteDistribution<T>> distributions)
+        {
+            this.distributions = distributions?.ToArray() ?? throw new ArgumentNullException(nameof(distributions));
+        }
+
+        public IEnumerable<T> Sample()
+        {
+            return distributions.Select(distribution => distribution.Sample());
+        }
+
+        public IEnumerable<IEnumerable<T>> Support()
+        {
+            return distributions.Select(d => d.Support()).CartesianProduct();
+        }
+
+        public int Weight(IEnumerable<T> item)
+        {
+            return item.Zip(distributions, (item, distribution) => distribution.Weight(item)).Product();
+        }
+    }
+}
