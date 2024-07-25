80ce246dd75a8fd4bc753e187a4150c081eaec6a
Timo Vesalainen <timovesalainen@hotmail.com>
2024-07-25 19:24:21 +0300

    Added extensions for enumerables and observables

--- Nintenlord/Collections/Foldable/FolderHelpers.cs
+++ Nintenlord/Collections/Foldable/FolderHelpers.cs
@@ -24,9 +24,19 @@ namespace Nintenlord.Collections.Foldable
             return folder.Transform(state);
         }
 
+        public static TOut FoldWith<TIn, TState, TOut>(this IEnumerable<TIn> enumerable, IFolder<TIn, TState, TOut> folder)
+        {
+            return folder.Fold(enumerable);
+        }
+
         public static IEnumerable<TOut> Scan<TIn, TState, TOut>(this IFolder<TIn, TState, TOut> folder, IEnumerable<TIn> enumerable)
         {
             return enumerable.Scan(folder.Start, folder.Fold).Select(folder.Transform);
+        }
+
+        public static IEnumerable<TOut> ScanWith<TIn, TState, TOut>(this IEnumerable<TIn> enumerable, IFolder<TIn, TState, TOut> folder)
+        {
+            return folder.Scan(enumerable);
         }
 
         public static async Task<TOut> FoldAsync<TIn, TState, TOut>(this IFolder<TIn, TState, TOut> folder, IAsyncEnumerable<TIn> enumerable)
@@ -47,6 +57,11 @@ namespace Nintenlord.Collections.Foldable
             return folder.Transform(state);
         }
 
+        public static async Task<TOut> FoldWithAsync<TIn, TState, TOut>(this IAsyncEnumerable<TIn> enumerable, IFolder<TIn, TState, TOut> folder)
+        {
+            return await folder.FoldAsync(enumerable);
+        }
+
         public static async IAsyncEnumerable<TOut> ScanAsync<TIn, TState, TOut>(this IFolder<TIn, TState, TOut> folder, IAsyncEnumerable<TIn> enumerable)
         {
             TState state = folder.Start;
@@ -59,9 +74,19 @@ namespace Nintenlord.Collections.Foldable
             }
         }
 
+        public static IAsyncEnumerable<TOut> ScanWithAsync<TIn, TState, TOut>(this IAsyncEnumerable<TIn> enumerable, IFolder<TIn, TState, TOut> folder)
+        {
+            return folder.ScanAsync(enumerable);
+        }
+
         public static IObservable<TOut> Fold<TIn, TState, TOut>(this IFolder<TIn, TState, TOut> folder, IObservable<TIn> observable)
         {
             return observable.Aggregate(folder.Start, folder.Fold).Select(folder.Transform);
+        }
+
+        public static IObservable<TOut> FoldWith<TIn, TState, TOut>(this IObservable<TIn> observable, IFolder<TIn, TState, TOut> folder)
+        {
+            return folder.Fold(observable);
         }
 
         public static IFolder<TIn, TState, TOut2> Select<TIn, TState, TOut1, TOut2>(this IFolder<TIn, TState, TOut1> folder, Func<TOut1, TOut2> selector)
